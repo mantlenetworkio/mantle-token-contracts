4fb004b54245761f88aaae6e6efeca6963b75b59
agnarsong <98442960+agnarsong@users.noreply.github.com>
2023-05-19 13:04:00 +0800

    feat: add some test cases

--- contracts/L2/L2MantleToken.sol
+++ contracts/L2/L2MantleToken.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 // L2MantleToken will continue use current implemation(https://github.com/mantlenetworkio/mantle/blob/main/packages/contracts/contracts/L2/predeploys/BVM_BIT.sol)
 // The implemation is copied from Optimism and audited.
 // No major modifications will be made, just need to make some minor changes such contract's name, L1 Mantle' contract address.
