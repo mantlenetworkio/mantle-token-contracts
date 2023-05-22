4234b27b5c2f276efdf74a8a89f0a1804e584712
afkbyte <afkbyte@protonmail.com>
2023-05-22 05:59:37 -0400

    made nessessary modifications

--- contracts/Migration/MantleTokenMigrator.sol
+++ contracts/Migration/MantleTokenMigrator.sol
@@ -1,148 +1,171 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.13;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "../interfaces/IERC20.sol";
 
-/**
- * @title   Contract to convert BIT (BITDAO) tokens to MNT (Mantle) tokens.
- *
- * @notice  Enable BIT token holders to convert their BIT tokens into MNT tokens.
- *
- * @dev     Provide the following functionalities:
- *          1. Deposit MNT (Mantle) tokens into this contract.
- *          2. Convert BIT tokens to MNT tokens using a pre-defined convertion rate 1 BIT = 3.14 MNT.
- *          3. Enable/disable conversion.
- */
-contract MantleTokenMigrator is Ownable {
-    using SafeERC20 for IERC20;
+contract MantleTokenMigrator {
 
-    /**
-     *  The conversion ratio is 3.14 = 314/100 (~Pi) and is specified by a denominator/numerator.
-     */
-    /** Denominator of conversion ratio. */
-    uint256 public constant CONVERSION_DENOMINATOR = 100;
-    /** Numerator of conversion ratio. */
-    uint256 public constant CONVERSION_NUMERATOR = 314;
+    /* ========== EVENTS ========== */
 
-    /// Events
-    event Migrate(address indexed from, uint256 mantleAmount);
+    // TokenSwap Events
+    event TokensMigrated(address indexed to, uint256 indexed amountOfBitSwapped, uint256 indexed amountOfMntRecieved);
 
-    /* ========== MIGRATION ========== */
+    // Contract State Events
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event ContractHalted(address indexed halter);
+    event ContractUnhalted(address indexed halter);
+    event TreasuryChanged(address indexed previousTreasury, address indexed newTreasury);
+
+    // Admin Events
+    event TokensSwept(address indexed token, address indexed recipient, uint256 amount);
+    event ContractFunded(address indexed funder, address indexed tokenAddress, uint256 amount);
+    event ContractDefunded(address indexed funder, address indexed tokenAddress, uint256 amount);
+
+    /* ========== ERRORS ========== */
+
+    error MantleTokenMigrator_OnlyOwner(address caller);
+    error MantleTokenMigrator_OnlyWhenNotHalted();
+    error MantleTokenMigrator_InsufficientContractBalance(address token, uint256 contractBalance, uint256 amountToTransfer);
+    error MantleTokenMigrator_TransferFailed(address token, uint256 amount);
+    error MantleTokenMigrator_SweepNotAllowed(address token);
+    error MantleTokenMigrator_InvalidFundingToken(address token);
+    error MantleTokenMigrator_FundingValueMismatch(address token, uint256 expectedAmount, uint256 actualAmount);
+    error MantleTokenMigrator_InvalidMessageData(bytes data);
+    error MantleTokenMigrator_EthNotAccepted();
+
+
     /* ========== STATE VARIABLES ========== */
 
-    /** The ERC20 BIT contract. Can only be set once. */
-    IERC20 public immutable bit;
-    /** The ERC-20 MNT contract. */
-    IERC20 public mantle;
+    address public immutable BIT_TOKEN_ADDRESS;
+    address public immutable MNT_TOKEN_ADDRESS;
 
-    /** Whether conversion is enabled or not. */
-    bool public enabled;
+    uint256 public immutable TOKEN_SWAP_RATIO;
+    uint256 public immutable TOKEN_SWAP_SCALING_FACTOR;
+    
+    address public treasury;
+    address public owner;
 
-    /**  Amount of BIT tokens converted by BIT token holders so far. */
-    uint256 public bitAmountMigrated;
-    /** Amount of MNT tokens credited to BIT token holders who have converted BIT tokens. */
-    uint256 public mantleAmountMigrated;
+    bool public halted;
 
-    /**
-     *  Set the address of the BIT token ERC-20 contract.
-     *
-     *  @dev    An arbitrary address can be passed as argument and will be
-     *          cast to en ERC20 contract.
-     *
-     *  @param  _bit    The address of the ERC-20 BIT token contract.
-     *
-     *  Requirements:
-     *      - `_bit` cannot be the zero address.
-     */
-    constructor(address _bit) {
-        require(_bit != address(0), "Initialisation of ERC-20 BIT contract cannot be zerobit");
-        bit = IERC20(_bit);
+    /* ========== MODIFIERS ========== */
+
+    modifier onlyOwner {
+        if (msg.sender != owner) revert MantleTokenMigrator_OnlyOwner(msg.sender);
+        _;
     }
 
-    /**
-     *  Enable users to convert BIT token to MNT tokens.
-     *
-     *  @dev    The token conversion comprises the following steps:
-     *          1.  transfer _bitAmount of BIT (`bit` ERC-20 token contract ) from `msg.sender` to `this`.
-     *          2.  transfer the corresponding amount of MNT (`mantle` ERC-20 token contract) from `this` to `msg.sender`.
-     *          As we are using a decimal conversion ratio, a small amount of MNT may be lost due to rounding.
-     *          For instance, converting 101 BIT results to MNT should yield 317.14 MNT which with interger division
-     *          is rounded down to 317. The amount lost a conversion is negilgible and always less than 1**-18 MNT.
-     *
-     *  @param  _bitAmount  The amount of BIT tokens to convert.
-     *
-     *  Requirements:
-     *      - conversion must be enabled
-     *      - the balance of `this` contract must be larger or equal than the amount of MNT to transfer.
-     *
-     */
-    function migrate(uint256 _bitAmount) external {
-        require(enabled, "Migration not enabled");
-
-        uint256 mantleAmount = (_bitAmount * CONVERSION_NUMERATOR) / CONVERSION_DENOMINATOR;
-
-        bit.safeTransferFrom(msg.sender, address(this), _bitAmount);
-
-        bitAmountMigrated = bitAmountMigrated + _bitAmount;
-        mantleAmountMigrated = mantleAmountMigrated + mantleAmount;
-        uint256 mantleAmountBalance = IERC20(mantle).balanceOf(address(this));
-        require(mantleAmount <= mantleAmountBalance, "Insufficient mantle tokens");
-
-        mantle.safeTransfer(msg.sender, mantleAmount);
-        emit Migrate(msg.sender, mantleAmount);
+    modifier onlyWhenNotHalted {
+        if (halted) revert MantleTokenMigrator_OnlyWhenNotHalted();
+        _;
     }
 
-    /**
-     *  Enable conversion for users.
-     *
-     *  Requirements:
-     *      - can only be set by owner.
-     */
-    function unpause() external onlyOwner {
-        enabled = true;
+    constructor(address _bitTokenAddress, address _mntTokenAddress, address _treasury, uint256 _tokenSwapRatio, uint256 _tokenSwapScalingFactor) {
+        owner = msg.sender;
+        halted = true;
+        
+        BIT_TOKEN_ADDRESS = _bitTokenAddress;
+        MNT_TOKEN_ADDRESS = _mntTokenAddress;
+
+        treasury = _treasury;
+
+        TOKEN_SWAP_RATIO = _tokenSwapRatio;
+        TOKEN_SWAP_SCALING_FACTOR = _tokenSwapScalingFactor;
     }
 
-    /**
-     *  Disable conversion for users.
-     *
-     *  Requirements:
-     *      - can only be set by owner.
-     */
-    function pause() external onlyOwner {
-        enabled = false;
+    /* ========== TOKEN SWAPPING ========== */
+
+    function swapAllBIT() onlyWhenNotHalted external {
+        uint256 amount = IERC20(BIT_TOKEN_ADDRESS).balanceOf(msg.sender);
+        _swapTokens(amount);
     }
 
-    /**
-     *  Set the MNT ERC-20 mantle address.
-     */
-    function setMantle(address _mantle) external onlyOwner {
-        require(address(mantle) == address(0), "Already set, only can be set once");
-        require(_mantle != address(0), "ERC-20 MNT contract cannot be zerobit");
-
-        mantle = IERC20(_mantle);
+    function swapBIT(uint256 _amount) onlyWhenNotHalted external {
+        _swapTokens(_amount);
     }
 
-    /**
-     *  Withdraw erc20 tokens.
-     *
-     *  Requirements:
-     *      - can only be set by owner.
-     */
-    function withdrawToken(address tokenAddress, uint256 amount, address recipient) external onlyOwner {
-        require(tokenAddress != address(0), "Token address cannot be 0x0");
-        require(amount > 0, "Withdraw value must be greater than 0");
-        if (recipient == address(0)) {
-            recipient = msg.sender; // if no address is specified the value will will be withdrawn to Owner
-        }
+    function _swapTokens(uint256 _amount) internal {
+        uint256 amountToSwap = _tokenSwapCalculation(_amount);
 
-        IERC20 tokenContract = IERC20(tokenAddress);
-        uint256 contractBalance = tokenContract.balanceOf(address(this));
-        if (amount > contractBalance) {
-            amount = contractBalance; // set the withdrawal amount equal to balance within the account.
-        }
+        // transfer user's BIT tokens to this contract
+        bool success = IERC20(BIT_TOKEN_ADDRESS).transferFrom(msg.sender, address(this), _amount);
+        if (success == false) revert MantleTokenMigrator_TransferFailed(BIT_TOKEN_ADDRESS, _amount);
 
-        tokenContract.safeTransfer(recipient, amount);
+        // transfer MNT tokens to user, if there are insufficient tokens, in the contract this will revert
+        success = IERC20(MNT_TOKEN_ADDRESS).transfer(msg.sender, amountToSwap);
+        if (success == false) revert MantleTokenMigrator_InsufficientContractBalance(MNT_TOKEN_ADDRESS, IERC20(MNT_TOKEN_ADDRESS).balanceOf(address(this)), amountToSwap);
+
+        emit TokensMigrated(msg.sender, _amount, amountToSwap);
     }
+
+    function _tokenSwapCalculation(uint256 _amount) internal returns (uint256) {
+        return (_amount * TOKEN_SWAP_RATIO) / TOKEN_SWAP_SCALING_FACTOR;
+    }
+
+    function tokenMigrationAmountToRecieve(uint256 _amount) external returns (uint256) {
+        return _tokenSwapCalculation(_amount);
+    }
+
+    /* ========== ADMIN UTILS ========== */
+
+    // Ownership Functions
+    function transferOwnership(address _newOwner) public onlyOwner {
+        owner = _newOwner;
+
+        emit OwnershipTransferred(msg.sender, _newOwner);
+    }
+
+    // Contract State Functions
+    function haltContract() public onlyOwner {
+        halted = true;
+
+        emit ContractHalted(msg.sender);
+    }
+
+    function unhaltContract() public onlyOwner {
+        halted = false;
+
+        emit ContractUnhalted(msg.sender);
+    }
+    function setTreasury(address _treasury) public onlyOwner {
+        address oldTreasury = treasury;
+        treasury = _treasury;
+
+        emit TreasuryChanged(oldTreasury, _treasury);
+    }
+
+    // Token Management Functions
+    function defundContract(address _tokenAddress, uint256 _amount) public onlyOwner {
+        if (_tokenAddress != BIT_TOKEN_ADDRESS && _tokenAddress != MNT_TOKEN_ADDRESS) revert MantleTokenMigrator_InvalidFundingToken(_tokenAddress);
+
+        // we can only defund BIT or MNT into the predefined treasury address
+        bool success = IERC20(_tokenAddress).transfer(treasury, _amount);
+        if (success == false) revert MantleTokenMigrator_TransferFailed(_tokenAddress, _amount);
+
+        emit ContractDefunded(treasury, _tokenAddress, _amount);
+    }
+
+    function sweepTokens (address _tokenAddress, address _recipient, uint256 _amount) public onlyOwner {
+        // we can only sweep tokens that are not BIT or MNT to an arbitrary addres
+        if ((_tokenAddress == address(BIT_TOKEN_ADDRESS)) || (_tokenAddress == address(MNT_TOKEN_ADDRESS))) revert MantleTokenMigrator_SweepNotAllowed(_tokenAddress);
+        bool success = IERC20(_tokenAddress).transfer(_recipient, _amount);
+        if (success == false) revert MantleTokenMigrator_TransferFailed(_tokenAddress, _amount);
+
+        emit TokensSwept(_tokenAddress, _recipient, _amount);
+    }
+
+    /* ========== FALLBACKS ========== */
+    fallback() external payable {
+        // calls sending ether to the contract without calldata will use the recieve() hook
+        if (msg.data.length != 0) revert MantleTokenMigrator_InvalidMessageData(msg.data);
+    }
+
+    receive() external payable {
+        // we do not accept ETH in this contract, so revert
+        // calls sending ether to this contract with valid calldata will revert because we have no payable functions
+        // @note you can force ETH into this contract with a selfdestruct, but it has no impact on the contract state
+        revert MantleTokenMigrator_EthNotAccepted();
+    }
+
+
+
 }
