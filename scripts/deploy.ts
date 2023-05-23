d724e59ce1df24a88d0a0a9a68880b88db7ce522
albert.zhang <shellteo@163.com>
2023-05-23 11:02:37 +0800

    update deploy script & verify contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,28 @@
 import { ethers } from 'hardhat';
 import { upgrades } from 'hardhat';
 import { Contract, ContractFactory } from 'ethers';
+import { MantleTokenMigrator__factory, L1MantleToken__factory } from "../typechain";
 
 async function main(): Promise<void> {
-  // const [deployer] = await ethers.getSigners();
+  const [deployer] = await ethers.getSigners();
+  const mintAmount = ethers.utils.parseEther("10000000000");
+  const L1MantleToken = new L1MantleToken__factory(deployer);
+  // deploy L1MantleToken
+  const l1MantleToken = await upgrades.deployProxy(L1MantleToken, [mintAmount, deployer.address])
+  await l1MantleToken.deployed();
 
-  const L1MantleToken: ContractFactory = await ethers.getContractFactory(
-    'L1MantleToken',
-  );
+  // BitDAO token address
+  const bitTokenAddress = '0x6d3d7b3779655405eE64662E33b79A4699845C3e'
 
-  // 0x0265B3921E3226aF7B2Fc431385b4157E0c762Db
-  const l1MantleToken = await upgrades.deployProxy(L1MantleToken, [10000000000000000000000000000n, 0x0265B3921E3226aF7B2Fc431385b4157E0c762Db], { initializer: 'initialize' })
+  //deploy MantleTokenMigrator
+  const MantleTokenMigrator = new MantleTokenMigrator__factory(deployer);
+  const mantleTokenMigrator = await MantleTokenMigrator.deploy(bitTokenAddress);
 
-  console.log(l1MantleToken.address, " l1MantleToken(proxy) address")
+  await mantleTokenMigrator.setMantle(l1MantleToken.address);
+  console.log('l1MantleToken(proxy) address: ', l1MantleToken.address)
+  console.log('mantleTokenMigrator address: ', mantleTokenMigrator.address)
+
+  //TODO: need transfer owner to multi-sig account
 }
 
 main()
