907004c018fc449c142cad44f71a221a214eb83e
afkbyte <afkbyte@protonmail.com>
2023-05-23 21:37:29 -0400

    fixed some issues and added natspec

--- contracts/Migration/MantleTokenMigrator.sol
+++ contracts/Migration/MantleTokenMigrator.sol
@@ -3,63 +3,149 @@ pragma solidity 0.8.13;
 
 import "../interfaces/IERC20.sol";
 
+/// @title A simulator for trees
+/// @author 0xMantle
+/// @notice Token migration contract for the BIT to MNT token migration
 contract MantleTokenMigrator {
 
     /* ========== STATE VARIABLES ========== */
 
+    /// @dev The address of the BIT token contract
     address public immutable BIT_TOKEN_ADDRESS;
+
+    /// @dev The address of the MNT token contract
     address public immutable MNT_TOKEN_ADDRESS;
 
-    uint256 public immutable TOKEN_SWAP_RATIO;
-    uint256 public immutable TOKEN_SWAP_SCALING_FACTOR;
+    /// @dev The numerator of the token conversion rate
+    uint256 public immutable TOKEN_CONVERSION_NUMERATOR;
+
+    /// @dev The denominator of the token conversion rate
+    uint256 public immutable TOKEN_CONVERSION_DENOMINATOR;
     
+    /// @dev The address of the treasury contract that receives defunded tokens
     address public treasury;
+
+    /// @dev The address of the owner of the contract
     address public owner;
 
+    /// @dev Boolean indicating if this contract is halted
     bool public halted;
 
     /* ========== EVENTS ========== */
 
     // TokenSwap Events
-    event TokensMigrated(address indexed to, uint256 indexed amountOfBitSwapped, uint256 indexed amountOfMntRecieved);
+
+    /// @dev Emitted when a user swaps BIT for MNT
+    /// @param to The address of the user that swapped BIT for MNT
+    /// @param amountOfBitSwapped The amount of BIT swapped
+    /// @param amountOfMntReceived The amount of MNT received
+    event TokensMigrated(address indexed to, uint256 amountOfBitSwapped, uint256 amountOfMntReceived);
 
     // Contract State Events
+
+    /// @dev Emitted when the owner of the contract is changed
+    /// @param previousOwner The address of the previous owner of this contract
+    /// @param newOwner The address of the new owner of this contract
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /// @dev Emitted when the contract is halted
+    /// @param halter The address of the caller that halted this contract
     event ContractHalted(address indexed halter);
+
+    /// @dev Emitted when the contract is unhalted
+    /// @param halter The address of the caller that unhalted this contract
     event ContractUnhalted(address indexed halter);
+
+    /// @dev Emitted when the treasury address is changed
+    /// @param previousTreasury The address of the previous treasury
+    /// @param newTreasury The address of the new treasury
     event TreasuryChanged(address indexed previousTreasury, address indexed newTreasury);
 
     // Admin Events
+
+    /// @dev Emitted when non BIT/MNT tokens are swept from the contract by the owner to the recipient address
+    /// @param token The address of the token contract that was swept
+    /// @param recipient The address of the recipient of the swept tokens
+    /// @param amount The amount of tokens swept
     event TokensSwept(address indexed token, address indexed recipient, uint256 amount);
-    event ContractFunded(address indexed funder, address indexed tokenAddress, uint256 amount);
-    event ContractDefunded(address indexed funder, address indexed tokenAddress, uint256 amount);
+
+    /// @dev Emitted when BIT/MNT tokens are defunded from the contract by the owner to the treasury
+    /// @param defunder The address of the defunder
+    /// @param token The address of the token contract that was defunded
+    /// @param amount The amount of tokens defunded
+    event ContractDefunded(address indexed defunder, address indexed token, uint256 amount);
 
     /* ========== ERRORS ========== */
 
+    /// @notice Thrown when the caller is not the owner and the function being called uses the {onlyOwner} modifier
+    /// @param caller The address of the caller
     error MantleTokenMigrator_OnlyOwner(address caller);
+
+    /// @notice Thrown when the contract is halted and the function being called uses the {onlyWhenNotHalted} modifier
     error MantleTokenMigrator_OnlyWhenNotHalted();
+
+    /// @notice Thrown when the input passed into the {_migrateTokens} function is zero
+    error MantleTokenMigrator_ZeroSwap();
+
+    /// @notice Thrown when at least one of the inputs passed into the constructor is a zero value
     error MantleTokenMigrator_ImproperlyInitialized();
+
+    /// @notice Thrown when the contract {token} balance is insufficient to transfer the amount of tokens requested
+    /// @param token The address of the token contract
+    /// @param contractBalance The balance of the token in this contract
+    /// @param amountToTransfer The amount of tokens requested to be transferred
     error MantleTokenMigrator_InsufficientContractBalance(address token, uint256 contractBalance, uint256 amountToTransfer);
+
+    /// @notice Thrown when a transfer of {token} from an external party fails
+    /// @param token The address of the token contract
+    /// @param amount The amount of tokens requested to be transferred
     error MantleTokenMigrator_TransferFailed(address token, uint256 amount);
+
+    /// @notice Thrown when the {_tokenAddress} passed into the {sweepTokens} function is the BIT or MNT token address
+    /// @param token The address of the token contract
     error MantleTokenMigrator_SweepNotAllowed(address token);
+
+    /// @notice Thrown when the {_tokenAddress} passed into the {defundContract} function is NOT the BIT or MNT token address
+    /// @param token The address of the token contract
     error MantleTokenMigrator_InvalidFundingToken(address token);
+
+
+    /// @notice Thrown when the contract receives a call with an invalid {msg}.data payload
+    /// @param data The msg.data payload
     error MantleTokenMigrator_InvalidMessageData(bytes data);
+
+    /// @notice Thrown when the contract receives a call with a non-zero {msg.value}
     error MantleTokenMigrator_EthNotAccepted();
 
     /* ========== MODIFIERS ========== */
 
+    /// @notice Modifier that checks that the caller is the owner of the contract
+    /// @dev Throws {MantleTokenMigrator_OnlyOwner} if the caller is not the owner
     modifier onlyOwner {
         if (msg.sender != owner) revert MantleTokenMigrator_OnlyOwner(msg.sender);
         _;
     }
 
+    /// @notice Modifier that checks that the contract is not halted
+    /// @dev Throws {MantleTokenMigrator_OnlyWhenNotHalted} if the contract is halted
     modifier onlyWhenNotHalted {
         if (halted) revert MantleTokenMigrator_OnlyWhenNotHalted();
         _;
     }
 
-    constructor(address _bitTokenAddress, address _mntTokenAddress, address _treasury, uint256 _tokenSwapRatio, uint256 _tokenSwapScalingFactor) {
-        if (_bitTokenAddress == address(0) || _mntTokenAddress == address(0) || _treasury == address(0) || _tokenSwapRatio== 0 || _tokenSwapScalingFactor == 0) revert MantleTokenMigrator_ImproperlyInitialized();
+    /// @notice Initializes the MantleTokenMigrator contract, setting the initial deployer as the contract owner
+    /// @dev _bitTokenAddress, _mntTokenAddress, _tokenConversionNumerator, and _tokenConversionDenominator are immutable: they can only be set once during construction
+    /// @dev the contract is initialized in a halted state
+    /// @dev Requirements: 
+    ///     - all parameters must be non-zero
+    ///     - _bitTokenAddress and _mntTokenAddress are assumed to have the same number of decimals
+    /// @param _bitTokenAddress The address of the BIT token contract
+    /// @param _mntTokenAddress The address of the MNT token contract
+    /// @param _treasury The address of the treasury contract that receives defunded tokens
+    /// @param _tokenConversionNumerator The numerator of the token conversion rate
+    /// @param _tokenConversionDenominator The denominator of the token conversion rate
+    constructor(address _bitTokenAddress, address _mntTokenAddress, address _treasury, uint256 _tokenConversionNumerator, uint256 _tokenConversionDenominator) {
+        if (_bitTokenAddress == address(0) || _mntTokenAddress == address(0) || _treasury == address(0) || _tokenConversionNumerator== 0 || _tokenConversionDenominator == 0) revert MantleTokenMigrator_ImproperlyInitialized();
 
         owner = msg.sender;
         halted = true;
@@ -69,36 +155,57 @@ contract MantleTokenMigrator {
 
         treasury = _treasury;
 
-        TOKEN_SWAP_RATIO = _tokenSwapRatio;
-        TOKEN_SWAP_SCALING_FACTOR = _tokenSwapScalingFactor;
+        TOKEN_CONVERSION_NUMERATOR = _tokenConversionNumerator;
+        TOKEN_CONVERSION_DENOMINATOR = _tokenConversionDenominator;
     }
 
 
     /* ========== FALLBACKS ========== */
+
+    /// @notice Fallback function that reverts if non-valid calldata is sent to the contract
     fallback() external payable {
-        // calls sending ether to the contract without calldata will use the recieve() hook
         if (msg.data.length != 0) revert MantleTokenMigrator_InvalidMessageData(msg.data);
     }
 
+    /// @notice Receive function that reverts if ETH is sent to the contract with a call
+    /// @dev This function is called whenever the contract receives ETH
+    /// @dev ETH can still be forced into this contract with a selfdestruct, but it has no impact on the contract state
     receive() external payable {
-        // we do not accept ETH in this contract, so revert
-        // calls sending ether to this contract with valid calldata will revert because we have no payable functions
-        // @note you can force ETH into this contract with a selfdestruct, but it has no impact on the contract state
+
         revert MantleTokenMigrator_EthNotAccepted();
     }
 
     /* ========== TOKEN SWAPPING ========== */
 
-    function swapAllBIT() onlyWhenNotHalted external {
+    /// @notice Swaps all of the caller's BIT tokens for MNT tokens
+    /// @dev emits a {TokensMigrated} event
+    /// @dev Requirements:
+    ///     - The caller must have approved this contract to spend their BIT tokens
+    ///     - The caller must have a non-zero balance of BIT tokens
+    function migrateAllBIT() onlyWhenNotHalted external {
         uint256 amount = IERC20(BIT_TOKEN_ADDRESS).balanceOf(msg.sender);
-        _swapTokens(amount);
+        _migrateTokens(amount);
     }
 
-    function swapBIT(uint256 _amount) onlyWhenNotHalted external {
-        _swapTokens(_amount);
+    /// @notice Swaps a specified amount of the caller's BIT tokens for MNT tokens
+    /// @dev emits a {TokensMigrated} event
+    /// @dev Requirements:
+    ///     - The caller must have approved this contract to spend at least {_amount} of their BIT tokens
+    ///     - The caller must have a balance of at least {_amount} of BIT tokens
+    /// @param _amount The amount of BIT tokens to swap
+    function migrateBIT(uint256 _amount) onlyWhenNotHalted external {
+        _migrateTokens(_amount);
     }
 
-    function _swapTokens(uint256 _amount) internal {
+    /// @notice Internal function that swaps a specified amount of the caller's BIT tokens for MNT tokens
+    /// @dev emits a {TokensMigrated} event
+    /// @dev Requirements:
+    ///     - The caller must have approved this contract to spend at least {_amount} of their BIT tokens
+    ///     - The caller must have a balance of at least {_amount} of BIT tokens
+    /// @param _amount The amount of BIT tokens to swap
+    function _migrateTokens(uint256 _amount) internal {
+        if (_amount == 0) revert MantleTokenMigrator_ZeroSwap();
+
         uint256 amountToSwap = _tokenSwapCalculation(_amount);
 
         // transfer user's BIT tokens to this contract
@@ -112,17 +219,26 @@ contract MantleTokenMigrator {
         emit TokensMigrated(msg.sender, _amount, amountToSwap);
     }
 
+    /// @notice Internal function that calculates the amount of MNT tokens to be recieved for a given amount of BIT tokens
+    /// @param _amount The amount of BIT tokens to swap
+    /// @return The amount of MNT tokens to be recieved
     function _tokenSwapCalculation(uint256 _amount) internal returns (uint256) {
-        return (_amount * TOKEN_SWAP_RATIO) / TOKEN_SWAP_SCALING_FACTOR;
+        return (_amount * TOKEN_CONVERSION_NUMERATOR) / TOKEN_CONVERSION_DENOMINATOR;
     }
 
-    function tokenMigrationAmountToRecieve(uint256 _amount) external returns (uint256) {
+    /// @notice Calculates the amount of MNT tokens to be recieved for a given amount of BIT tokens
+    /// @param _amount The amount of BIT tokens to swap
+    /// @return The amount of MNT tokens to be recieved
+    function tokenMigrationAmountToReceive(uint256 _amount) external returns (uint256) {
         return _tokenSwapCalculation(_amount);
     }
 
     /* ========== ADMIN UTILS ========== */
 
     // Ownership Functions
+
+    /// @notice Transfers ownership of the contract to a new address
+    /// @dev emits an {OwnershipTransferred} event
     function transferOwnership(address _newOwner) public onlyOwner {
         owner = _newOwner;
 
@@ -130,17 +246,25 @@ contract MantleTokenMigrator {
     }
 
     // Contract State Functions
+
+    /// @notice Halts the contract, preventing token migrations
+    /// @dev emits a {ContractHalted} event
     function haltContract() public onlyOwner {
         halted = true;
 
         emit ContractHalted(msg.sender);
     }
 
+    /// @notice Unhalts the contract, allowing token migrations
+    /// @dev emits a {ContractUnhalted} event
     function unhaltContract() public onlyOwner {
         halted = false;
 
         emit ContractUnhalted(msg.sender);
     }
+
+    /// @notice Sets the treasury address
+    /// @dev emits a {TreasuryChanged} event
     function setTreasury(address _treasury) public onlyOwner {
         address oldTreasury = treasury;
         treasury = _treasury;
@@ -149,6 +273,15 @@ contract MantleTokenMigrator {
     }
 
     // Token Management Functions
+
+    /// @notice Defunds the contract by transferring a specified amount of BIT or MNT tokens to the treasury address
+    /// @dev emits a {ContractDefunded} event
+    /// @dev Requirements:
+    ///     - The caller must be the contract owner
+    ///     - {_tokenAddress} must be either the BIT or the MNT token address
+    ///     - The contract must have a balance of at least {_amount} of {_tokenAddress} tokens
+    /// @param _tokenAddress The address of the token to defund
+    /// @param _amount The amount of tokens to defund
     function defundContract(address _tokenAddress, uint256 _amount) public onlyOwner {
         if (_tokenAddress != BIT_TOKEN_ADDRESS && _tokenAddress != MNT_TOKEN_ADDRESS) revert MantleTokenMigrator_InvalidFundingToken(_tokenAddress);
 
@@ -159,6 +292,15 @@ contract MantleTokenMigrator {
         emit ContractDefunded(treasury, _tokenAddress, _amount);
     }
 
+    /// @notice Sweeps a specified amount of tokens to an arbitrary address
+    /// @dev emits a {TokensSwept} event
+    /// @dev Requirements:
+    ///     - The caller must be the contract owner
+    ///     - {_tokenAddress} must not the BIT or the MNT token address
+    ///     - The contract must have a balance of at least {_amount} of {_tokenAddress} tokens
+    /// @param _tokenAddress The address of the token to sweep
+    /// @param _recipient The address to sweep the tokens to
+    /// @param _amount The amount of tokens to sweep
     function sweepTokens (address _tokenAddress, address _recipient, uint256 _amount) public onlyOwner {
         // we can only sweep tokens that are not BIT or MNT to an arbitrary addres
         if ((_tokenAddress == address(BIT_TOKEN_ADDRESS)) || (_tokenAddress == address(MNT_TOKEN_ADDRESS))) revert MantleTokenMigrator_SweepNotAllowed(_tokenAddress);
